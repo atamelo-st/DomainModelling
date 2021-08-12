12437033b2b7baa9d71e6ee5a154f62f364a634a
Andrey Tamelo <atamelo@servicetitan.com>
2021-08-12 14:31:27 -0700

    fixing typos

--- DomainModelling/DomainModelling/DataAccessLayer/CalendarRepository.cs
+++ DomainModelling/DomainModelling/DataAccessLayer/CalendarRepository.cs
@@ -25,7 +25,7 @@ namespace DomainModelling.DataAccessLayer
 
         private string GetSql(IEnumerable<DomainEvent> domainEvents)
         {
-            var sb = new StringBuilder("BRGIN TRANSACTION");
+            var sb = new StringBuilder("BEGIN TRANSACTION");
 
             foreach (DomainEvent domainEvent in domainEvents)
             {
@@ -33,43 +33,43 @@ namespace DomainModelling.DataAccessLayer
                 {
                     case DomainEvent.RegularEventAdded regularEventAdded:
                         {
-                            this.ProcessDomainEvent(regularEventAdded, sb);
+                            ProcessDomainEvent(regularEventAdded, sb);
                             break;
                         }
 
                     case DomainEvent.RegularEventUpdated regularEventUpdated:
                         {
-                            this.ProcessDomainEvent(regularEventUpdated, sb);
+                            ProcessDomainEvent(regularEventUpdated, sb);
                             break;
                         }
 
                     case DomainEvent.RegularEventDeleted regularEventDeleted:
                         {
-                            this.ProcessDomainEvent(regularEventDeleted, sb);
+                            ProcessDomainEvent(regularEventDeleted, sb);
                             break;
                         }
 
                     case DomainEvent.RecurringEventAdded recurringEventAdded:
                         {
-                            this.ProcessDomainEvent(recurringEventAdded, sb);
+                            ProcessDomainEvent(recurringEventAdded, sb);
                             break;
                         }
 
                     case DomainEvent.RecurringEventUpdated recurringEventUpdated:
                         {
-                            this.ProcessDomainEvent(recurringEventUpdated, sb);
+                            ProcessDomainEvent(recurringEventUpdated, sb);
                             break;
                         }
 
                     case DomainEvent.RecurringEventOccurrenceUpdated recurringEventOcurrenceUpdated:
                         {
-                            this.ProcessDomainEvent(recurringEventOcurrenceUpdated, sb);
+                            ProcessDomainEvent(recurringEventOcurrenceUpdated, sb);
                             break;
                         }
 
                     case DomainEvent.RecurringEventOccurrenceDeleted recurringEventOccurrenceDeleted:
                         {
-                            this.ProcessDomainEvent(recurringEventOccurrenceDeleted, sb);
+                            ProcessDomainEvent(recurringEventOccurrenceDeleted, sb);
                             break;
                         }
 
@@ -82,37 +82,37 @@ namespace DomainModelling.DataAccessLayer
             return sb.ToString();
         }
 
-        private void ProcessDomainEvent(DomainEvent.RecurringEventOccurrenceDeleted recurringEventOccurrenceDeleted, StringBuilder sb)
+        private static void ProcessDomainEvent(DomainEvent.RecurringEventOccurrenceDeleted recurringEventOccurrenceDeleted, StringBuilder sb)
         {
             sb.AppendLine($"SQL Imitation for {recurringEventOccurrenceDeleted.GetType().Name}");
         }
 
-        private void ProcessDomainEvent(DomainEvent.RecurringEventOccurrenceUpdated recurringEventOccurrenceUpdated, StringBuilder sb)
+        private static void ProcessDomainEvent(DomainEvent.RecurringEventOccurrenceUpdated recurringEventOccurrenceUpdated, StringBuilder sb)
         {
             sb.AppendLine($"SQL Imitation for {recurringEventOccurrenceUpdated.GetType().Name}");
         }
 
-        private void ProcessDomainEvent(DomainEvent.RecurringEventUpdated recurringEventUpdated, StringBuilder sb)
+        private static void ProcessDomainEvent(DomainEvent.RecurringEventUpdated recurringEventUpdated, StringBuilder sb)
         {
             sb.AppendLine($"SQL Imitation for {recurringEventUpdated.GetType().Name}");
         }
 
-        private void ProcessDomainEvent(DomainEvent.RecurringEventAdded recurringEventAdded, StringBuilder sb)
+        private static void ProcessDomainEvent(DomainEvent.RecurringEventAdded recurringEventAdded, StringBuilder sb)
         {
             sb.AppendLine($"SQL Imitation for {recurringEventAdded.GetType().Name}");
         }
 
-        private void ProcessDomainEvent(DomainEvent.RegularEventDeleted regularEventDeleted, StringBuilder sb)
+        private static void ProcessDomainEvent(DomainEvent.RegularEventDeleted regularEventDeleted, StringBuilder sb)
         {
             sb.AppendLine($"SQL Imitation for {regularEventDeleted.GetType().Name}");
         }
 
-        private void ProcessDomainEvent(DomainEvent.RegularEventUpdated regularEventUpdated, StringBuilder sb)
+        private static void ProcessDomainEvent(DomainEvent.RegularEventUpdated regularEventUpdated, StringBuilder sb)
         {
             sb.AppendLine($"SQL Imitation for {regularEventUpdated.GetType().Name}");
         }
 
-        private void ProcessDomainEvent(DomainEvent.RegularEventAdded regularEventAdded, StringBuilder sb)
+        private static void ProcessDomainEvent(DomainEvent.RegularEventAdded regularEventAdded, StringBuilder sb)
         {
             sb.AppendLine($"SQL Imitation for {regularEventAdded.GetType().Name}");
         }
